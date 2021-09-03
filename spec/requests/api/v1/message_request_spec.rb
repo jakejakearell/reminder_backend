c0adbfcb523ba0d654a7b846d08ddb601f18f231
Jacob Arellano <jmm.arellano@gmail.com>
2021-09-03 10:28:36 -0600

    Assert path for message

---
+++ spec/requests/api/v1/message_request_spec.rb
@@ -0,0 +1,20 @@
+describe "User Creation" do
+  describe 'Happy Paths' do
+    it "can create a message" do
+      body = {
+                 email: 'jake@ihopethiswork.org',
+                 user_id: '1',
+                 message: 'iasdf a sdfasf asd'
+               }
+
+      headers = {"CONTENT_TYPE" => "application/json", 'ACCEPT' => 'application/json' }
+
+      post "/api/v1/message", headers: headers, params: body, as: :json
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+
+      # message_json = JSON.parse(response.body, symbolize_names: true)
+    end
+  end
+end
