42c10515b6bbb1fc14b950de6592bd9eba50e148
Jacob Arellano <jmm.arellano@gmail.com>
2021-09-30 09:38:39 -0600

    Add validations/testing

--- spec/models/reminder_spec.rb
+++ spec/models/reminder_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Reminder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it {should validate_presence_of(:end_date)}
+    it {should validate_presence_of(:start_date)}
+    it {should validate_presence_of(:time)}
+    it {should validate_presence_of(:message)}
+    it {should validate_presence_of(:user_id)}
+    it {should validate_presence_of(:email)}
+  end
 end
